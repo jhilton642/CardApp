7770dd9e49f2c21995c4dcc33068aefc4e6d8c82
jhilton642 <joshua.hilton@att.net>
2020-09-28 11:22:29 -0500

    Add project files.

---
+++ CardApp/Card.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardApp
+{
+    class Card
+    {
+        string rank;
+        string suit;
+        int val;
+        public string Rank { get => rank; set => rank = value; }
+        public string Suit { get => suit; set => suit = value; }
+        public int Val { get => val; set => val = value; }
+
+        public Card(string rank, string suit, int value)
+        {
+            this.rank = rank;
+            this.suit = suit;
+            this.val = value;
+        }
+        public override string ToString()
+        {
+            return rank + " " + suit;
+        }
+        public int Value
+        {
+            get
+            {
+                return val;
+            }
+            set
+            {
+                val = value;
+            }
+        }
+    }
+}
---
+++ CardApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Card aceOfSpades = new Card("A","♠",14);
+            Console.WriteLine(aceOfSpades);
+            string[] ranks = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A" };
+            string[] suits = { "♥", "♦", "♣", "♠" };
+
+            List <Card> deck = new List<Card>();
+            for (int r = 0; r < ranks.Length; r++)
+            {
+                for (int s = 0; s < suits.Length; s++)
+                {
+                    Card card = new Card(ranks[r], suits[s], r + 1);
+                    deck.Add(card);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
